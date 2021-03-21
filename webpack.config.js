9af6fe459fd6b55bfe2136d430af7c9c3bfa841c
Patrick Lima <patrickm.lima1@gmail.com>
2021-03-21 00:40:19 -0400

    feat: Add webpack support for build

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const copyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const { NODE_ENV = 'production' } = process.env;
+
+module.exports = {
+  entry: './src/app.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  node: {
+    __dirname: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.yaml', '.yml'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ya?ml$/,
+        type: 'json',
+        use: 'yaml-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new copyPlugin({
+      patterns: [
+        { from: 'spec-docs', to: 'spec-docs', toType: 'dir' },
+        { from: '.env', to: '.env', toType: 'file' }
+      ]
+    })
+  ]
+}
