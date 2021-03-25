dd7154073652879b91ae36152cd84c1a84595c0f
Patrick Lima <patrickm.lima1@gmail.com>
2021-03-24 20:40:28 -0400

    feat: Change default route

--- src/api/controllers/default.ts
+++ src/api/controllers/default.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, Router } from 'express';
+import config from '../../config/env';
 
 const router: Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
     return res.status(200).json({
-        message: 'NodeJS Boilerplate - Server is running'
+        message: 'NodeJS Typescript Boilerplate - Server is running',
+        documentation: `http://${config.app.ip}:${config.app.port}/api-docs`
     });
 });
 
--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,6 @@ import defaultRoute from './controllers/default';
 import docsRoute from './controllers/apiDocs';
 
 export default (appInstance: Application) => {
-    appInstance.use('/api/', defaultRoute);
+    appInstance.use('/', defaultRoute);
     appInstance.use('/api-docs', docsRoute);
 }
