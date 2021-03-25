162ba29585dd01bf3c5bcc65b82618038167b98e
Patrick Lima <patrickm.lima1@gmail.com>
2021-03-24 22:17:13 -0400

    style: Edit to avoid lint error

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -8,9 +8,9 @@ console.log(`doc index file path: ${docIndexFile}`);
 
 const docsFile = YAML.load(docIndexFile);
 
-let serverUrl = docsFile['servers'][0]['url'].replace('HOST_IP', config.app.ip)
+let serverUrl = docsFile.servers[0].url.replace('HOST_IP', config.app.ip)
 serverUrl = serverUrl.replace('HOST_PORT', config.app.port);
 
-docsFile['servers'][0]['url'] = serverUrl;
+docsFile.servers[0].url = serverUrl;
 
 export const getDocsAsObject = () => docsFile;
